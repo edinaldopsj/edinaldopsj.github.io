5c288178892c4245ff6dcb66d77a1dac61da860f
Edinaldo <edinaldopsj@hotmail.com.br>
2018-07-25 07:26:28 -0300

    Correção formatacao

--- facerec/src/App.js
+++ facerec/src/App.js
@@ -70,9 +70,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Particles className='particles' 
-          params={particlesOptions}
-        />
+        <Particles className='particles' params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
